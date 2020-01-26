54b05db8f0f527e5d35259db2680b5c01f43fc7c
master8 <master8.michael@gmail.com>
2020-01-27 00:44:46 +0500

    text search

--- app/src/main/java/com/master8/shana/ui/MainActivity.kt
+++ app/src/main/java/com/master8/shana/ui/MainActivity.kt
@@ -3,9 +3,10 @@ package com.master8.shana.ui
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.master8.shana.data.repository.MoviesRepositoryImpl
 import com.master8.shana.data.source.tmdb.createTMDbApiService
 import com.master8.shana.databinding.ActivityMainBinding
-import kotlinx.coroutines.Dispatchers
+import com.master8.shana.domain.usecase.SearchMoviesUseCase
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 
@@ -16,9 +17,9 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        MainScope().launch(Dispatchers.IO) {
-            val result = createTMDbApiService().multipleSearch("vinland")
-            Log.d("mv8", "after ${result}")
+        MainScope().launch {
+            val search = SearchMoviesUseCase(MoviesRepositoryImpl(createTMDbApiService()))
+            Log.d("mv8", "after ${search("シャナ")}")
         }
     }
 }
