26c72cff528f55d01e45b9880e33d4e73e9883fe
master8 <master8.michael@gmail.com>
2020-02-04 22:27:12 +0500

    setup base theme

--- app/src/main/java/com/master8/shana/app/App.kt
+++ app/src/main/java/com/master8/shana/app/App.kt
@@ -1,10 +1,12 @@
 package com.master8.shana.app
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
     }
 }
