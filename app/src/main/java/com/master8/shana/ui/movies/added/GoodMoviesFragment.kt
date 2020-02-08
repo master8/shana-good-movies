134070ca5b55d33179b446dd57182998011f0559
master8 <master8.michael@gmail.com>
2020-02-08 20:35:54 +0500

    move common code to AddMoviesFragment

--- app/src/main/java/com/master8/shana/ui/movies/added/GoodMoviesFragment.kt
+++ app/src/main/java/com/master8/shana/ui/movies/added/GoodMoviesFragment.kt
@@ -1,48 +1,11 @@
 package com.master8.shana.ui.movies.added
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.firebase.ui.database.FirebaseRecyclerOptions
-import com.master8.shana.app.app
-import com.master8.shana.app.dependency.ViewModelFactory
-import com.master8.shana.data.repository.converters.parseMovie
-import com.master8.shana.databinding.FragmentMoviesBinding
-import com.master8.shana.domain.entity.Movie
-import com.master8.shana.ui.movies.MovieViewModel
-import com.master8.shana.ui.movies.MoviesFirebaseAdapter
+import com.google.firebase.database.Query
+import com.master8.shana.data.source.firebase.database.FirebaseRealtimeDatabase
 
-class GoodMoviesFragment : Fragment() {
+class GoodMoviesFragment : AddedMoviesFragment() {
 
-    val viewModel by viewModels<MovieViewModel> { ViewModelFactory(requireContext()) }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val binding = FragmentMoviesBinding.inflate(inflater, container, false)
-
-        val firebaseRealtimeDatabase = requireContext().app.moviesModule.firebaseRealtimeDatabase
-
-        val options = FirebaseRecyclerOptions.Builder<Movie>()
-            .setQuery(firebaseRealtimeDatabase.goodMovies.orderByChild("dateAdded"), ::parseMovie)
-            .setLifecycleOwner(this)
-            .build()
-
-        val adapter = MoviesFirebaseAdapter(options, viewModel, viewLifecycleOwner)
-
-        val layoutManager = LinearLayoutManager(requireContext()).apply {
-            reverseLayout = true
-            stackFromEnd = true
-        }
-        binding.listMovies.layoutManager = layoutManager
-        binding.listMovies.adapter = adapter
-
-        return binding.root
+    override fun getMoviesQuery(database: FirebaseRealtimeDatabase): Query {
+        return database.goodMovies
     }
 }
