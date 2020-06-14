9d17a68e6ab3f7a2e6920bbf7194f249da0d19f2
master8 <master8.michael@gmail.com>
2020-06-14 10:17:04 +0300

    temp fix

--- app/src/main/java/com/master8/shana/ui/movies/dialog/MovieDialogViewModel.kt
+++ app/src/main/java/com/master8/shana/ui/movies/dialog/MovieDialogViewModel.kt
@@ -59,7 +59,9 @@ class MovieDialogViewModel(
 
     fun changeMoviePoster(poster: Uri) = viewModelScope.launch {
         _selectedMove.value?.let {
-            _selectedMove.value = changeMoviePosterUseCase(it, poster)
+//            _selectedMove.value = changeMoviePosterUseCase(it, poster)
+            changeMoviePosterUseCase(it, poster)
+            _closeDialog.value = Event(true)
         }
     }
 
