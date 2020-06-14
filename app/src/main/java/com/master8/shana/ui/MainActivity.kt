f7df797584c3376695fc5c8974c8877518a6efbc
master8 <master8.michael@gmail.com>
2020-06-14 10:33:01 +0300

    add anonymous auth

--- app/src/main/java/com/master8/shana/ui/MainActivity.kt
+++ app/src/main/java/com/master8/shana/ui/MainActivity.kt
@@ -2,15 +2,30 @@ package com.master8.shana.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.master8.shana.R
 import com.master8.shana.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_Shana_DayNight)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = Firebase.auth
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if (auth.currentUser == null) {
+            auth.signInAnonymously()
+        }
     }
 }
